53fb1aa399745234344d4d5ec543513ba658ae0d
Luis <luis.delgadillo@inmersys.com>
2019-10-29 23:12:18 -0600

    Se ha programado la pantalla de pausa. Queda pendiente replicarla en los otros 9 niveles.

--- Assets/Scripts/Pausa.cs
+++ Assets/Scripts/Pausa.cs
@@ -7,16 +7,27 @@ public class Pausa : MonoBehaviour{
     [Header("REFERENCIAS")]
     public AudioSource SFX_Salida;
     public AudioSource SFX_Siguiente;
-
+    public GameObject PantallaPausa;
     private bool pausa = false;
+    BGMController BGM;
+
+    private void Start(){
+        PantallaPausa.SetActive(false);
+        BGM = GameObject.Find("BGM").GetComponent<BGMController>();        
+    }
 
     public void TogglePausa(){
         if (!pausa){
             pausa = true;
+            PantallaPausa.SetActive(true);
+            BGM.GetComponent<AudioSource>().volume = 0.15f;
             Time.timeScale = 0;
             SFX_Salida.Play();
-        }else{
+        }
+        else{
             pausa = false;
+            PantallaPausa.SetActive(false);
+            BGM.GetComponent<AudioSource>().volume = BGM.volumenMaximo;
             Time.timeScale = 1;
             SFX_Siguiente.Play();
         }
